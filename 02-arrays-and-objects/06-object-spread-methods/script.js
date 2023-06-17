caebcbe9d756528c1c0a4f26bcd838afa8d9661d
Grant <gbentley0618@comcast.net>
2023-06-16 19:05:56 -0500

    section 2 module 6 compleat

---
+++ 02-arrays-and-objects/06-object-spread-methods/script.js
@@ -0,0 +1,13 @@
+//Ways to make an object
+
+/* 1.object litteral
+     Connst todo = {}; 
+    
+2. Object constructer */
+    const todo = new Object();
+
+    todo.id = 1;
+    todo.name = 'Buy Milk';
+    todo.compleated = false;
+
+    console.log(todo);
