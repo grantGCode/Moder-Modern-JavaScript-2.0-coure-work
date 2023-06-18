e11a4bb7a3814840b1a147dd3e5eff281c1731c4
Grant <gbentley0618@comcast.net>
2023-06-18 15:45:42 -0500

    ection 3 module 2 compleat

--- 03-functions-scope/02-params-arguments/script.js
+++ 03-functions-scope/02-params-arguments/script.js
@@ -20,3 +20,29 @@ function sum(...numbers) {
 /*with the rest Param ... we can have the function return an Array of values it returns. */
 console.log(sum(1, 2, 3));
 
+// Object as params
+function loginUser(user){
+    return `The user ${user.name} with the id of ${user.id} is loged in`;
+}
+
+const user = {
+    id: 1,
+    name: 'John',
+}; 
+
+
+
+loginUser(user); // Reminder I'm adding the argument of Varibale User not the Param in the function
+
+
+// Arrays as Params
+
+function getRandom (arr) {
+    const randomeIndex = Math.floor(Math.random() * arr.length);
+
+    const item = arr [randomeIndex];
+
+    console.log(item);
+}// function will cosole log any number in the Array I in () as a agument when funtion is run.
+
+getRandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
