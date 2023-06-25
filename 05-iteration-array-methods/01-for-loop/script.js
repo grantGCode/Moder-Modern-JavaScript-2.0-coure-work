406669a0eef8b87ffb24f59de52b4f912b5b5786
Grant <gbentley0618@comcast.net>
2023-06-25 16:16:12 -0500

    Section 5 Module 1 complete

--- 05-iteration-array-methods/01-for-loop/script.js
+++ 05-iteration-array-methods/01-for-loop/script.js
@@ -5,3 +5,48 @@
 // CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
+
+
+// Basic for loop
+
+    for (let i = 0; i <= 10; i++) {
+        console.log('Number' + i);
+    }
+    
+    /* If I were to change the number value of the condition expression the loop would count till that number reached */
+
+    /* 
+
+// For loop with an if statment
+
+    // for (let i = 0; i <= 10; i++) {
+    //     if (i === 7) {
+    //     console.log('7 is my lucky number');
+    //     } else {
+    //         console.log('Number' + i);
+    //     }
+    // }
+
+// Nested for loops
+
+    // for (let i = 0; i <= 10; i++) {
+    //     console.log('Number ' + i);
+        
+    //     for (let j = 1; j <= 10; j++) {
+    //     console.log(`${i} * ${j} = ${i * j}`);
+    //     }
+    // }
+
+// Loop through an array
+
+const namesArray = ["John", "Emily", "Michael", "Sophia", "William"];
+
+    for (let i = 0; i < namesArray.length; i++) {
+        if (namesArray[i] === "Michael") {
+            console.log("Michael (is the BEST)");
+            } else {
+            console.log(namesArray[i]);
+        } 
+        
+    }
+/* The if statment is a challenge if there were no if statment the for loop stament would be a console log of namesArray[i] */
