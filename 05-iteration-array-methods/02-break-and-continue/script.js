d7928b8103a4ced7fcd6437859d36f7528073cb5
Grant <gbentley0618@comcast.net>
2023-06-25 16:56:22 -0500

    Section 5 Module 2 complete

---
+++ 05-iteration-array-methods/02-break-and-continue/script.js
@@ -0,0 +1,27 @@
+// Breraking (Used to Brake out of a loop.)
+
+// for (i = 0; i <= 20; i++) {
+//     if (i === 15) {
+//     console.log("Breaking the loop...")
+//     break;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+/* The loop with stop at 15 according to the if stament and thanks to the Break command on line 15. */
+
+
+// Continue 
+
+    for (i = 0; i <= 20; i++) {
+            if (i === 3) {
+                console.log("What Number?");
+                continue;
+            }
+        console.log(i);
+    }
+
+/* Lets say I was working for VALVE software and I need to wright a loop to count to 20. To have the loop skip
+Number 3 and keep counting add the if stament on lines 18 -21 with the key word command of "continue" to have the
+loop count till 20 */ 
