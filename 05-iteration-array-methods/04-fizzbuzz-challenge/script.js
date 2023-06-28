eadfe8017a08550c9c38aae400758f553a821f07
Grant <gbentley0618@comcast.net>
2023-06-28 18:15:40 -0500

    Section 5 Module 4 complete

--- 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -2,11 +2,15 @@
 
 for (let i = 1; i <= 100; i++) {
     
-    // if (i = ) {
-        
-    //     console.log(i + ' FizzBuzz');
-        
-    // }
-    console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i + ' FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log(i + ' Fizz');
+    }else if (i % 5 === 0 ) {
+        console.log(i + ' Buzz');
+    } else {
+        console.log(i);
+
+    } 
     
 }
