3d2e3a89316a77b9278d9b6b20eeb452941e677b
Grant <gbentley0618@comcast.net>
2023-07-07 00:24:06 -0500

    Section 6 Module 10 notes

--- 06-document-object-model/10-insert-elements/shopping-list/script.js
+++ 06-document-object-model/10-insert-elements/shopping-list/script.js
@@ -1,12 +1,44 @@
 // insertAdjacentElement Example
 
+function insertElement() {
+  const filter = document.querySelector('.filter');
+
+  const h1 = document.createElement('h1')
+  h1.textContent = 'insertAdjacentElement';
+  h1.style.color = 'red';
+  h1.style.fontSize = 'small';
+  h1.style.margin = 'small';
+
+  filter.insertAdjacentElement('beforebegin', h1);
+}
 // insertAdjacentText Example
+
+function insertText() {
+  const item = document.querySelector('li:first-child');
+
+  item.insertAdjacentText('afterend', 'insertAdjacentText')
+}
 
 // insertAdjacentHTML example
 
+function insertHTML() {
+  const clearBtn = document.querySelector('#clear');
+
+  clearBtn.insertAdjacentHTML('afterend', '<h2>insertAdjacentHTML</h2>');
+
+}
+
 // insertBefore Example
 
+function insertBeforeItem() {
 
+  
+}
+
+
+insertElement();
+insertText();
+insertHTML();
 /*
 <!-- beforebegin -->
 <p>
