b41a9b4b711df9e87df787168e45ddea9451519b
Grant <gbentley0618@comcast.net>
2023-07-09 13:33:54 -0500

    Section 7 Module 3 Complete

--- 07-events/03-event-object/shopping-list/script.js
+++ 07-events/03-event-object/shopping-list/script.js
@@ -1,3 +1,29 @@
+const  logo = document.querySelector('img');
+
+function events (ent) {
+    // console.log(ent.target);
+    // console.log(ent.curentTarget);
+    // console.log(ent.type);
+    // console.log('x ', ent.clientX, ' y ', ent.clientY);
+    //console.log('x ', ent.offsetX, ' y ', ent.offsetY);
+    // console.log('x ', ent.pageX, ' y ', ent.pageY);
+    // console.log('x ', ent.screenX, ' y ', ent.screenY);
+    // console.log(ent.timeStamp);
+}
+
+function onDrag(ent) {
+    document.querySelector('h1').textContent = `x ${ent.clientX} Y ${ent.clientY}`;
+}
+
+console.log(document.querySelector('h1'));
+
+logo.addEventListener('click', events);
+logo.addEventListener('drag', onDrag)
+
+// logo.addEventListener('click', function (ent) {
+//     console.log(ent);
+// });
+
 /*
 - `target` - The element that triggered the event
 - `currentTarget` - The element that the event listener is attached to (These are the same in this case
