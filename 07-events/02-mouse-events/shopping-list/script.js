604bfd87dedcf4c5431a2893082242b16cc07d8e
Grant <gbentley0618@comcast.net>
2023-07-09 13:01:09 -0500

    Section 7 Module 2 Update

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,6 +1,16 @@
 const  logo = document.querySelector('img');
 const onClick = () => console.log('click event');
-
+const onDubbleClick = () => console.log('Dubble click event');
+const onRightClick = () => console.log('Right click event');
+const onMouseDown = () => console.log('Mouse Down event');
+const onMouseUp = () => console.log('Mouse Up event');
+const onMousWheel = () => console.log('"Whell in the sky keeps on turning"');
+const onMouseOver = () => console.log('Mouse Over event');
+const onMouseOut = () => console.log('Mouse Out event');
+const onDragStart = () => console.log('Mouse Start event');
+const onDragStop = () => console.log('Mouse Stop event');
+const onDrag = () => console.log('Drag event');
+const onDragEnd = () => console.log('Drag End event');
 
 
 // Dark Mode
@@ -27,14 +37,18 @@ const darkMode = () => {
 }
 
 
-//Event Liseriner
+//Event Liseriners
 
 logo.addEventListener('click', onClick);
-// logo.addEventListener('dblclick', onDubbleClick);
-darkButton.addEventListener('click', darkMode)
-
-
-
-
-
-
+logo.addEventListener('dblclick', onDubbleClick);
+darkButton.addEventListener('click', darkMode);
+logo.addEventListener('contextmenu', onRightClick);
+logo.addEventListener('mousedown', onMouseDown);
+logo.addEventListener('mouseUp', onMouseUp);
+logo.addEventListener('wheel', onMousWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('mouseout', onMouseOut);
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('dragstop', onDragStop);
+logo.addEventListener('drag', onDrag);
+logo.addEventListener('dragend', onDragEnd);
