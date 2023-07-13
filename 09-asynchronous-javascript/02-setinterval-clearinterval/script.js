46b15a80a2c430de2728b3b3d01542b98f5f2b01
Grant <gbentley0618@comcast.net>
2023-07-13 17:34:59 -0500

    Section 9 Module 2 Notes

---
+++ 09-asynchronous-javascript/02-setinterval-clearinterval/script.js
@@ -0,0 +1,32 @@
+let intervalID;
+
+function startChange() {
+    if (!intervalID) {
+        intervalID = setInterval(changeRandomColor, 3000);
+        console.log('ran')
+    }
+}
+
+function changeColor() {
+    if (document.body.style.backgroundColor !== 'black') {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+}
+
+
+function stopChange() {
+    clearInterval(intervalID);
+}
+function changeRandomColor() {
+    const  randomColor = Math.floor(Math.random() * 16777215).toString(16)
+    document.body.style.backgroundColor = `#${randomColor}`;
+    const  randomTextColor = Math.floor(Math.random() * 16777215).toString(16)
+    document.body.style.color = `#${randomTextColor }`;
+}
+
+document.getElementById('start').addEventListener('click', startChange);
+document.getElementById('stop').addEventListener('click', stopChange);
