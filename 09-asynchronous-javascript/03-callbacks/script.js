a20102d4b1ea151591da6cba2e296495808fd4d1
Grant <gbentley0618@comcast.net>
2023-07-13 18:22:54 -0500

    Section 9 Module 3 Notes

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -4,3 +4,34 @@ function toggle(e) {
 }
 
 document.querySelector('button').addEventListener('click', toggle);
+
+
+// Bellow will mimic fetching posts from a server or an http request
+
+const posts = [
+  {title: 'Post One', body: 'This is post one'},
+  {title: 'Post Two', body: 'This is post two'}
+];
+
+
+
+function createPost(post, cb) {
+  setTimeout(() => {
+    post.push(post);
+    cb();
+  }, 2000); // cb = call back (also know as call back function) 
+}
+
+function getPosts() {
+  setTimeout(() => {
+    postMessage.forEach(function (post) {
+      const div = document.createElement('div');
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      document.querySelector('#posts').appendChild(div); 
+    });
+  }, 1000);  
+}
+
+
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+
