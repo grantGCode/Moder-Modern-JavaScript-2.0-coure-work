5a2a057b9b2bcb200e1f5a0364eb00e608b3a990
Grant <gbentley0618@comcast.net>
2023-07-16 16:56:10 -0500

    Section 9 Module 9 notes

--- 09-asynchronous-javascript/09-promise-chaining/script.js
+++ 09-asynchronous-javascript/09-promise-chaining/script.js
@@ -1,6 +1,6 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let error = false;
+    let error = false; // change this to true and none of the .thens will run on lines 14 - 24 only the .catch will run and the . then under line 25 after the catch
 
     if (!error) {
       resolve({ name: 'John', age: 30 });
@@ -13,5 +13,17 @@ const promise = new Promise((resolve, reject) => {
 promise
   .then((user) => {
     console.log(user);
+    return user.name;// Pulls the Name fron the object on line 8
   })
-  .catch((error) => console.log(error))
+  .then((name) => {
+    console.log(name);// param on line 18 & 19 will pull the name from line 16
+    return name.length;
+  })
+  .then((nameLength) => {
+    console.log(nameLength);
+  })
+  .catch((error) => { 
+  console.log(error);
+  return 123;
+})
+.then((x) => console.log('This will run no matter what', x));// this will run if retun or error ocours
