7ef3e75dd37234f63c92cc282502206fd03865af
Grant <gbentley0618@comcast.net>
2023-07-18 23:36:26 -0500

    Section 10 Module 9 Notes

--- 10-fetch-and-async-await/09-multiple-promises-async-await/script.js
+++ 10-fetch-and-async-await/09-multiple-promises-async-await/script.js
@@ -19,16 +19,58 @@ function getData(endpoint) {
   });
 }
 
-getData('./movies.json')
-  .then((movies) => {
-    console.log(movies);
-    return getData('./actors.json');
-  })
-  .then((actors) => {
-    console.log(actors);
-    return getData('./directors.json');
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+// getData('./movies.json')
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData('./actors.json');
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData('./directors.json');
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => console.log(error));
+
+
+async function getAllData() {
+  const movies = await getData('./movies.json');
+  const actors = await getData('./actors.json');
+  const directors = await getData('./directors.json');
+  console.log(movies, actors, directors);
+  console.log('ran after 37 - 34 function');
+}
+getAllData();
+
+console.log('ran before line 37 - 34 function');
+
+
+async function getAllDataWithFetch() {
+  const moviesRes = await fetch()
+  const movies = await moviesRes.json();
+  const actorsRes = await fetch()
+  const actors = await actorsRes.json();
+  const directorsRes = await fetch()
+  const directors = await directorsRes.json();
+  
+  console.log(movies, actors, directors);
+  
+}
+
+async function getAllDataPromiseAll() {
+  const [moviesRes, actorsRes, directorsRes] = await Promise.all([
+    fetch('./movies.json'),
+    fetch('./actors.json'),
+    fetch('./directors.json')
+  ]);
+
+  const movies = await moviesRes.json();
+  const actors = await actorsRes.json();
+  const directors = await directorsRes.json();
+
+  console.log(movies, actors, directors);
+}
+
+getAllDataPromiseAll();
+
