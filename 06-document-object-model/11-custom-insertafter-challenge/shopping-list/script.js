eb66b2aa0411288ee039189ac12082e0fadf75d3
Grant Bentley <gbentley0618@comcast.net>
2024-10-07 15:03:33 -0500

    Update .js Notes for Section 06 Module 11

--- 06-document-object-model/11-custom-insertafter-challenge/shopping-list/script.js
+++ 06-document-object-model/11-custom-insertafter-challenge/shopping-list/script.js
@@ -1,15 +1,17 @@
-/* Custom inserAfter-challenege */
+/* Custom insertAfter-challenge */
 
 function insertAfter(newEl, existingEl) {
     existingEl.parentElement.insertBefore(newEl, existingEl.nextSibling);
     
-}
+};
 
 
-
+// New element to inert
 const li = document.createElement('li');
 li.textContent = 'Insert Me After!';
 
+// Existing element to insert after
 const firstItem = document.querySelector('li:first-child');
 
+// My custom function called
 insertAfter(li, firstItem);
