fda305d0356f208bea45ff50ddf4a577ab116fab
Grant Bentley <gbentley0618@comcast.net>
2024-10-08 11:35:09 -0500

    update .js notes for Section 06 Module 14

--- 06-document-object-model/14-styles-classes/script.js
+++ 06-document-object-model/14-styles-classes/script.js
@@ -6,35 +6,41 @@ const items = itemList.querySelectorAll('li');
 
 
 function run() {
-    //calls Name
+    /* className */
     // console.log(itemList.className); // Showing what I have select
-    // text.className = 'card dark';
+    // text.className = 'card dark'; 
+    /* 
+        This will overwrite the previous class "card" it replaced
+        with dark. to keep the "card" class pass in card as you
+        see above on line: 11. 
+    */
 
-    //classList
-    // console.log(itemList.classList); 
+    /* classList property */
+     console.log(itemList.classList); // return DOMToken list which is an array of class.
 
-    itemList.classList.forEach((c) => console.log(c));
+    itemList.classList.forEach((c) => console.log(c)); // return each class to console
 
         //text.classList.add('dark');
         //text.classList.remove('card');
         
-        
+        /* Best way of implementing a dark light mode switcher */
         // text.classList.toggle('dark');
         // text.classList.toggle('hidden');
 
-        text.classList.replace('card', 'dark');
+        text.classList.replace('card', 'dark'); // Using replace to toggle and replace one class with another.
 
-    // Change Style
+    /* Change Style */
     // itemList.style.lineHeight = '3';
     
+    /* Looping through the <ul> */
     items.forEach((items, index) => {
     itemList.style.color = 'red';
-
+        /* Targeting specific item with index */
         if (index === 2) {
             items.style.color = 'blue';
-        }
+        };
     });
-}
+};
 
 
 document.querySelector('button').onclick = run;
